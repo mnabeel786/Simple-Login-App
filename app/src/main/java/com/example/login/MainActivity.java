5086edbbfb14cfea31cbaf84b71d9cc6009f6ddc
Mohd Nabeel <mohd.nabeelansari@gmail.com>
2019-02-23 01:48:21 +0530

    Simple Login App

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 public class MainActivity extends AppCompatActivity {
 
     public EditText name;
