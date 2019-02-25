6ca7b3959adad55c5b74ff0942f7e75c7958e23d
Mohd Nabeel <mohd.nabeelansari@gmail.com>
2019-02-25 17:10:02 +0530

    Simple Login App

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -6,38 +6,42 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     public EditText name;
-    public EditText password;
+    public EditText pass;
     public Button login;
+    TextView tvRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        name = (EditText)findViewById(R.id.etUsername);
+        pass = (EditText)findViewById(R.id.etPassword);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validate(name.getText().toString(), pass.getText().toString());
+            }
+        });
+
+        tvRegister = (TextView)findViewById(R.id.textViewR);
+        tvRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerIntent = new Intent(MainActivity.this,NextActivity.class);
+                startActivity(registerIntent);
+            }
+        });
     }
 
-     public void authenticateLogin(View v){
+    private void validate (String username, String password){
 
-         name=(EditText)findViewById(R.id.username);
-         password=(EditText)findViewById(R.id.password);
-         login=(Button)findViewById(R.id.login_button);
-
-         validate(name.getText().toString(), password.getText().toString());
-     }
-    private void validate(String username, String password){
-
-        if((username.equals("admin"))&&(password.equals("1234"))){
-            Intent intent = new Intent(MainActivity.this, NextActivity.class);
-            intent.putExtra("username", name.getText().toString());
-            startActivity(intent);
-        }
-        else{
-            Toast.makeText(getApplicationContext(), "This is not a correct username or password", Toast.LENGTH_LONG).show();
-        }
-
-        }
+    }
 
     }
--- app/src/main/java/com/example/login/NextActivity.java
+++ app/src/main/java/com/example/login/NextActivity.java
@@ -1,22 +1,99 @@
 package com.example.login;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class NextActivity extends AppCompatActivity {
 
-    TextView textView4;
-    String name;
+    private TextView tvLogin;
+    private EditText userName, userEmail, userPassword, num;
+    private Button register;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
+        setupUIViews();
 
-        textView4 = (TextView)findViewById(R.id.textView4);
-        name = getIntent().getStringExtra("username");
+        firebaseAuth = FirebaseAuth.getInstance();
 
-        textView4.setText(name);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(validate()){
+                    String user_email = userEmail.getText().toString().trim();
+                    String user_pass = userPassword.getText().toString().trim();
+
+                    firebaseAuth.createUserWithEmailAndPassword(user_email, user_pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if(task.isSuccessful()){
+                                Toast.makeText(NextActivity.this, "Registration Successful", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent (NextActivity.this, MainActivity.class));
+                            }else{
+                                Toast.makeText(NextActivity.this, "Registration Failed", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+        tvLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(NextActivity.this, MainActivity.class));
+            }
+        });
+
+
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner1);
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(NextActivity.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.names));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(myAdapter);
+    }
+
+
+    private void setupUIViews(){
+        userName = (EditText)findViewById(R.id.name);
+        userEmail = (EditText)findViewById(R.id.email);
+        num = (EditText)findViewById(R.id.mob_num);
+        userPassword = (EditText)findViewById(R.id.pass);
+        register = (Button)findViewById(R.id.sign_up);
+        tvLogin = (TextView)findViewById(R.id.textView7);
+    }
+    private boolean validate(){
+        Boolean result = false;
+
+        String name = userName.getText().toString();
+        String password = userPassword.getText().toString();
+        String email = userEmail.getText().toString();
+        String number = num.getText().toString();
+
+        if(name.isEmpty() && password.isEmpty() && email.isEmpty() && number.isEmpty()){
+
+            Toast.makeText(this, "Please provide all the details", Toast.LENGTH_SHORT).show();
+        }else{
+            result = true;
+        }
+        return result;
     }
 }
